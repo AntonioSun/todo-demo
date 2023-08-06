405e89d97dfe5f581b58e1f4d3ecf4b7ab59ff16
Antonio Sun <asun@ats.l5.ca>
2023-08-06 17:46:02 -0400

    - [+] add docs/

---
+++ docs/app-worker.js
@@ -0,0 +1,117 @@
+const cacheName = "app-" + "b3817922720c8ba7fbdfdaecc6bb5e3aa87f2247";
+const resourcesToCache = ["/todo-demo","/todo-demo/app.css","/todo-demo/app.js","/todo-demo/manifest.webmanifest","/todo-demo/wasm_exec.js","/todo-demo/web/app.wasm","/todo-demo/web/icon-192.png","/todo-demo/web/icon-512.png","/todo-demo/web/styles.css"];
+
+self.addEventListener("install", (event) => {
+  console.log("installing app worker b3817922720c8ba7fbdfdaecc6bb5e3aa87f2247");
+
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        return cache.addAll(resourcesToCache);
+      })
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  console.log("app worker b3817922720c8ba7fbdfdaecc6bb5e3aa87f2247 is activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("push", (event) => {
+  if (!event.data || !event.data.text()) {
+    return;
+  }
+
+  const notification = JSON.parse(event.data.text());
+  if (!notification) {
+    return;
+  }
+
+  const title = notification.title;
+  delete notification.title;
+
+  if (!notification.data) {
+    notification.data = {};
+  }
+  let actions = [];
+  for (let i in notification.actions) {
+    const action = notification.actions[i];
+
+    actions.push({
+      action: action.action,
+      path: action.path,
+    });
+
+    delete action.path;
+  }
+  notification.data.goapp = {
+    path: notification.path,
+    actions: actions,
+  };
+  delete notification.path;
+
+  event.waitUntil(self.registration.showNotification(title, notification));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const notification = event.notification;
+  let path = notification.data.goapp.path;
+
+  for (let i in notification.data.goapp.actions) {
+    const action = notification.data.goapp.actions[i];
+    if (action.action === event.action) {
+      path = action.path;
+      break;
+    }
+  }
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (var i = 0; i < clientList.length; i++) {
+          let client = clientList[i];
+          if ("focus" in client) {
+            client.focus();
+            client.postMessage({
+              goapp: {
+                type: "notification",
+                path: path,
+              },
+            });
+            return;
+          }
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow(path);
+        }
+      })
+  );
+});
